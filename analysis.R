05a6305aa94b0d188433e8d55287c4b7d57d5bc2
mm24aow <mm24aow@herts.ac.uk>
2025-12-05 15:22:38 +0000

    load dataset

---
+++ analysis.R
@@ -0,0 +1,8 @@
+# Project Title: B-130 Group Project
+# File Name: analysis.R
+# Dataset:
+# nba_2017_twitter_players.csv
+# Sections:
+# 1. Load dataset
+data <- read.csv("/Users/saim/Desktop/B-130/B-130/nba_2017_twitter_players.csv")
+head(data)
