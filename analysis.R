fd48ebc3304b79587d201c3515ed0e70ae1ff937
mm24aow <mm24aow@herts.ac.uk>
2025-12-06 11:36:40 +0000

    Load dataset, inspect data, and create histogram of favorites

--- analysis.R
+++ analysis.R
@@ -6,4 +6,21 @@
 # 1. Load dataset
 data <- read.csv("nba_2017_twitter_players.csv")
 head(data)
+str(data)
+summary(data)
 
+x <- data$TWITTER_FAVORITE_COUNT
+y <- data$TWITTER_RETWEET_COUNT
+
+# Scatter plot
+plot(x, y,
+     main = "Twitter Favorites vs Retweets",
+     xlab = "Favorite Count",
+     ylab = "Retweet Count",
+     pch = 19, col = "blue")
+
+abline(lm(y ~ x), col = "red", lwd = 2)
+
+hist(x,
+     main = "Distribution of Twitter Favorite Counts",
+     xlab = "Favorite Count")
